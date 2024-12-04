a6c077fa6f266f98b845c4f3080f5903f995969c
Joshua SIX <joshua.six@etud.univ-jfc.fr>
2024-12-04 09:22:55 +0100

    modifications pour amandine

--- src/main/java/org/git/Ojahsu/Main.java
+++ src/main/java/org/git/Ojahsu/Main.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Main extends Canvas {
 
     public static void main(String[] args) {
-        System.out.printf("Hello AMANDINE comment ca va ?!");
+        System.out.print("Hello AMANDINE comment ca va ?!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
