293b28fe523685d2952019c4ca66d11ec6b2506f
Mani0726 <157810331+Mani0726@users.noreply.github.com>
2024-12-04 09:35:13 +0100

    Commit d'Amandine Modification du Syso + du for

--- src/main/java/org/git/Ojahsu/Main.java
+++ src/main/java/org/git/Ojahsu/Main.java
@@ -5,10 +5,10 @@ import java.awt.*;
 public class Main extends Canvas {
 
     public static void main(String[] args) {
-        System.out.print("Hello AMANDINE comment ca va ?!");
-
+        //System.out.print("Hello AMANDINE comment ca va ?!");
+        System.out.print("Hello JOSHUAAA ca va très bien et toi ?!");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println("i = " + i+5);
         }
     }
 }
